cf8a663a7996cae12814c66d71658b8f38bd093b
brunoJorquera <jorquera.b18@gmail.com>
2021-10-21 17:28:59 -0700

    1-5 complete

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class EvenAndOdd extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ""
+        }
+    }
+
+    handleChange(input){
+        this.setState({ userInput: input })
+    }
+    handleClick(userInput){
+        let array = userInput.split(',');
+        let evens = [];
+        let odds = [];
+
+        for(let i = 0; i < array.length; i++){
+            if(array[i] % 2 === 0){
+                evens.push((array[i]));
+            } else{
+                odds.push((array[i]));
+            }
+        }
+
+        this.setState({ evenArray: evens, oddArray: odds })
+    }
+
+    render() {
+        return (
+            <div className="puzzleBox evenAndOddPB">
+                <h4> Even and Odds</h4>
+               <input 
+               className="inputLine"
+               type="text"
+               onChange={ (e) => this.handleChange(e.target.value) }
+               />
+               <button 
+               className="confirmationButton" 
+               onClick={ () => {this.handleClick(this.state.userInput)} }
+               >Click Here</button>
+               <span className="resultsBox">evens: {this.state.evenArray}</span>
+               <span className="resultsBox">odds: {this.state.oddArray}</span>
+            </div>
+        )
+    }
+}
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+export default class FilterObject extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            unFilteredArray: [
+                {id:1,name:"carlos",age:10, bread:"white"},
+                {id:2, name:"chinchkilla",age:20},
+                {id:3,name:"chinchdrilla", car:"chevy"}
+            ],
+            userInput: "",
+            filteredArray: []
+        }
+    };
+
+    handleChange = (input) => {
+        this.setState({ userInput: input })
+    };
+    handleClick = (userInput) => {
+        const clickArr = this.state.unFilteredArray.filter( (el) =>
+             (el.hasOwnProperty(userInput)) );
+          
+        this.setState({ filteredArray: clickArr });
+    };
+
+    render() {
+        return (
+            <div className="puzzleBox filterObjectPB">
+                <h4> Filter Object</h4>
+                <span className="puzzleText">
+                Unfiltered Array:
+                <br/> 
+                {/* {this.state.unFilteredArray.map(( bum ) => <li key={bum.id}>{bum.name}:__{bum.age}y/o__ID#:{bum.id}</li>)} */}
+                {JSON.stringify(this.state.unFilteredArray)}
+                </span>
+                <input 
+                onChange={ (e) => this.handleChange( e.target.value ) }
+                className="inputLine" 
+                />
+                <button
+                onClick={() => {this.handleClick(this.state.userInput)}}
+                className="confirmationButton"
+                >Click</button>
+                <span className="resultsBox filterObjectRB">
+                Filtered Array:
+                <br/> 
+                {/* {this.state.filteredArray.map(( data ) => <li key={data.id}>{data.name}</li>)} */}
+                {JSON.stringify(this.state.filteredArray)}
+                </span>
+            </div>
+        )
+    };
+};
